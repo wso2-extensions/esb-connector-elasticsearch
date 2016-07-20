e7bb872fbcaaeac75d281e936025dd0213291c66
vives <vsvivevives@gmail.com>
2016-07-20 10:58:11 +0530

    Update ElasticsearchConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/elasticsearch/ElasticsearchConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/elasticsearch/ElasticsearchConnectorIntegrationTest.java
@@ -45,7 +45,7 @@ public class ElasticsearchConnectorIntegrationTest extends ConnectorIntegrationT
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("elasticsearch-connector-1.0.1-SNAPSHOT");
+        init("elasticsearch-connector-1.0.1");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
